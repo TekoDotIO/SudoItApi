069eb76c536ee7d3df0a6bd0f6cad056f901f3d9
1003 fengye <mcfengye1003@outlook.com>
2022-01-23 21:37:33 +0800

    修改版本信息

--- SudoItApi/AppInfo.cs
+++ SudoItApi/AppInfo.cs
@@ -3,8 +3,8 @@
     public class AppInfo
     {
         public static string AppName = "$udo!T Api Server";
-        public static string Version = "v.1.0.0.0";
-        public static string InsideVersion = "Beta 0001";
+        public static string Version = "v.1.0.0.1";
+        public static string InsideVersion = "Beta 0002";
         public static string Copyright = "EachOther Tech. 2022";
         public static string UpdateLog = "- The first beta version of $udo!T Api";
     }
