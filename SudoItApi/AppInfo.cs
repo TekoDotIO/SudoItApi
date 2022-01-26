425625d2fdb47f0112e905916ca94f69b5c37ad1
1003 fengye <mcfengye1003@outlook.com>
2022-01-26 12:20:11 +0800

    修改更新日志 -增加进程模块翻页 -增加注释

--- SudoItApi/AppInfo.cs
+++ SudoItApi/AppInfo.cs
@@ -6,10 +6,10 @@
     public class AppInfo
     {
         public static string AppName = "$udo!T Api Server";
-        public static string Version = "v.1.0.0.3";
-        public static string InsideVersion = "Beta 0005";
+        public static string Version = "v.1.0.0.4";
+        public static string InsideVersion = "Beta 0006";
         public static string Copyright = "EachOther Tech. 2022";
-        public static string UpdateLog = "-修复status里面CPU占用始终为0的问题;\n-添加注释;\n-现在,非致命的异常将被标为Exception并返回异常信息;\n-系统状态新增总内存与已用内存.";
+        public static string UpdateLog = "-增加进程模块翻页\n-增加注释";
     }
     /// <summary>
     /// 用户信息类(定制)
